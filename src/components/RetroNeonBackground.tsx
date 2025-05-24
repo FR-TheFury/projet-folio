42286f2e00b896909fe4fbd941bd23d420e56962
Téo Debay <frthefury@gmail.com>
2025-05-24 22:43:51 +0200

    fond

--- src/components/RetroNeonBackground.tsx
+++ src/components/RetroNeonBackground.tsx
@@ -8,7 +8,7 @@ const RetroNeonBackground: React.FC = () => {
     <div 
       className="fixed inset-0 z-[-1] w-full h-full bg-cover bg-center bg-no-repeat"
       style={{
-        backgroundImage: 'url(/mon-fond.jpg)'
+        backgroundImage: 'url(/background.jpeg)'
       }}
     />
   );
